376be049f2d7d7f6f29cb42b685baa524d711dd5
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-14 11:15:36 +0530

    Initial Welcome Program

---
+++ src/com/employeewage/Employee.java
@@ -0,0 +1,4 @@
+package com.employeewage;
+
+public class Employee {
+}
---
+++ src/com/employeewage/EmployeeWageApp.java
@@ -0,0 +1,10 @@
+package com.employeewage;
+
+public class EmployeeWageApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Compuatation Program");
+    }
+
+}
