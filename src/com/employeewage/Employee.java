7ea24a66f43df38efe3277313dee2ea910ceaf27
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-14 11:21:30 +0530

    UC1 Employee Attendance using class and object

--- src/com/employeewage/Employee.java
+++ src/com/employeewage/Employee.java
@@ -1,4 +1,22 @@
 package com.employeewage;
 
 public class Employee {
+
+    private boolean present;
+
+    public void checkAttendance() {
+
+        int empCheck = (int)(Math.random()*2);
+
+        if(empCheck == 1) {
+            present = true;
+        }
+        else {
+            present = false;
+        }
+    }
+
+    public boolean isPresent() {
+        return present;
+    }
 }
