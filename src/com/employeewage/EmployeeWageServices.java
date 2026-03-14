73dde5c5c0769f83374116a59c324916a85e4553
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-14 12:10:48 +0530

    UC4 Refractored using switch case

--- src/com/employeewage/EmployeeWageServices.java
+++ src/com/employeewage/EmployeeWageServices.java
@@ -4,15 +4,23 @@ public class EmployeeWageServices {
 
     public static final int WAGE_PER_HOUR = 20;
 
-    public int getWorkingHours(int empType) {
-
+    public int getWorkingHours(int empType)
+    {
         int hours = 0;
 
-        if(empType == 1)
-            hours = 4;   // Part Time
+        switch(empType)
+        {
+            case 1:
+                hours = 4;
+                break;
 
-        else if(empType == 2)
-            hours = 8;   // Full Time
+            case 2:
+                hours = 8;
+                break;
+
+            default:
+                hours = 0;
+        }
 
         return hours;
     }
