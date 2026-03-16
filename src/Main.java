657f337989d27e88058674102fc9226dc3757ce6
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-16 15:44:33 +0530

    UC6 - Calculate Wages till condition of total working hours or days is reached

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,23 @@ public class Main {
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        // UC5 - Calculating Wages for a Month - Assume 20 Working Day per Month
+        // UC6 - Calculate Wages till a condition of total working hours or days is reached - Assume 100 hours and 20 days
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOURS = 8;
         final int PART_TIME_HOURS = 4;
-        final int WORKING_DAYS_PER_MONTH = 20;
+        final int MAX_HOURS = 100;
+        final int MAX_DAYS = 20;
 
         int totalWage = 0;
+        int totalHours = 0;
+        int totalDays = 0;
 
-        for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+        while (totalHours < MAX_HOURS && totalDays < MAX_DAYS) {
+            totalDays++;
             int empCheck = (int)(Math.random() * 2); // 0 or 1
 
             if (empCheck == 1) {
-                System.out.println("Day " + day + ": Employee is Present");
+                System.out.println("Day " + totalDays + ": Employee is Present");
                 int empType = (int)(Math.random() * 2); // 0 - Part Time, 1 - Full Time
                 int hours = 0;
                 switch (empType) {
@@ -27,16 +31,19 @@ public class Main {
                         hours = FULL_DAY_HOURS;
                         break;
                 }
+                totalHours += hours;
                 int dailyWage = WAGE_PER_HOUR * hours;
                 totalWage += dailyWage;
                 System.out.println("Daily Employee Wage: " + dailyWage);
             } else {
-                System.out.println("Day " + day + ": Employee is Absent");
+                System.out.println("Day " + totalDays + ": Employee is Absent");
                 System.out.println("Daily Employee Wage: 0");
             }
         }
 
-        System.out.println("Total Monthly Wage: " + totalWage);
+        System.out.println("Total Days: " + totalDays);
+        System.out.println("Total Hours: " + totalHours);
+        System.out.println("Total Wage: " + totalWage);
 
     }
 }
