18ea40e3e3b7343ad0538fa5652c973c0c14b3ba
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-16 15:46:58 +0530

    UC7 - Refactor the Code to write a Class Method to Compute Employee Wage

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -1,16 +1,16 @@
+package com.employeewage;
 
-public class Main {
-    public static void main(String[] args) {
+public class EmployeeWage {
 
-        System.out.println("Welcome to Employee Wage Computation Program");
+    // Class Variables
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 4;
+    public static final int MAX_HOURS = 100;
+    public static final int MAX_DAYS = 20;
 
-        // UC6 - Calculate Wages till a condition of total working hours or days is reached - Assume 100 hours and 20 days
-        final int WAGE_PER_HOUR = 20;
-        final int FULL_DAY_HOURS = 8;
-        final int PART_TIME_HOURS = 4;
-        final int MAX_HOURS = 100;
-        final int MAX_DAYS = 20;
-
+    // Class Method to Compute Employee Wage
+    public static int computeEmployeeWage() {
         int totalWage = 0;
         int totalHours = 0;
         int totalDays = 0;
@@ -45,5 +45,6 @@ public class Main {
         System.out.println("Total Hours: " + totalHours);
         System.out.println("Total Wage: " + totalWage);
 
+        return totalWage;
     }
 }
---
+++ src/com/employeewage/Main.java
@@ -0,0 +1,12 @@
+package com.employeewage;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+        // UC7 - Refactor the Code to write a Class Method to Compute Employee Wage
+        EmployeeWage.computeEmployeeWage();
+
+    }
+}
