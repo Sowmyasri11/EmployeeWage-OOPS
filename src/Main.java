10d5c625f938e7787417ca91fd02753c9cc6b64c
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-16 15:27:00 +0530

    UC1 - Check Employee is Present or Absent using RANDOM

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.printf("Hello and welcome!");
 
-        
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+        // UC1 - Check Employee is Present or Absent
+        int empCheck = (int)(Math.random() * 2); // 0 or 1
+
+        if (empCheck == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
     }
 }
