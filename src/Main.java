5b63207031e95152bb0132dab3096bda75147f1f
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-16 15:42:35 +0530

    UC5 - Calculating Wages for a Month

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,39 @@ public class Main {
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        // UC1, UC2, UC3 & UC4 - Check Employee is Present or Absent and Calculate Daily Wage with Part Time using Switch Case
+        // UC5 - Calculating Wages for a Month - Assume 20 Working Day per Month
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOURS = 8;
         final int PART_TIME_HOURS = 4;
+        final int WORKING_DAYS_PER_MONTH = 20;
 
-        int empCheck = (int)(Math.random() * 2); // 0 or 1
+        int totalWage = 0;
 
-        if (empCheck == 1) {
-            System.out.println("Employee is Present");
-            int empType = (int)(Math.random() * 2); // 0 - Part Time, 1 - Full Time
-            int hours = 0;
-            switch (empType) {
-                case 0:
-                    hours = PART_TIME_HOURS;
-                    break;
-                case 1:
-                    hours = FULL_DAY_HOURS;
-                    break;
+        for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+            int empCheck = (int)(Math.random() * 2); // 0 or 1
+
+            if (empCheck == 1) {
+                System.out.println("Day " + day + ": Employee is Present");
+                int empType = (int)(Math.random() * 2); // 0 - Part Time, 1 - Full Time
+                int hours = 0;
+                switch (empType) {
+                    case 0:
+                        hours = PART_TIME_HOURS;
+                        break;
+                    case 1:
+                        hours = FULL_DAY_HOURS;
+                        break;
+                }
+                int dailyWage = WAGE_PER_HOUR * hours;
+                totalWage += dailyWage;
+                System.out.println("Daily Employee Wage: " + dailyWage);
+            } else {
+                System.out.println("Day " + day + ": Employee is Absent");
+                System.out.println("Daily Employee Wage: 0");
             }
-            int dailyWage = WAGE_PER_HOUR * hours;
-            System.out.println("Daily Employee Wage: " + dailyWage);
-        } else {
-            System.out.println("Employee is Absent");
-            System.out.println("Daily Employee Wage: 0");
         }
+
+        System.out.println("Total Monthly Wage: " + totalWage);
 
     }
 }
