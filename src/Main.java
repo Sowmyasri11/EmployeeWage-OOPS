7cf1080f33459846877a1e2b279ab47bcc0a82c6
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-16 15:40:52 +0530

    UC4 - Solving using Switch Case Statement

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        // UC1, UC2 & UC3 - Check Employee is Present or Absent and Calculate Daily Wage with Part Time
+        // UC1, UC2, UC3 & UC4 - Check Employee is Present or Absent and Calculate Daily Wage with Part Time using Switch Case
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOURS = 8;
         final int PART_TIME_HOURS = 4;
@@ -14,7 +14,15 @@ public class Main {
         if (empCheck == 1) {
             System.out.println("Employee is Present");
             int empType = (int)(Math.random() * 2); // 0 - Part Time, 1 - Full Time
-            int hours = (empType == 1) ? FULL_DAY_HOURS : PART_TIME_HOURS;
+            int hours = 0;
+            switch (empType) {
+                case 0:
+                    hours = PART_TIME_HOURS;
+                    break;
+                case 1:
+                    hours = FULL_DAY_HOURS;
+                    break;
+            }
             int dailyWage = WAGE_PER_HOUR * hours;
             System.out.println("Daily Employee Wage: " + dailyWage);
         } else {
