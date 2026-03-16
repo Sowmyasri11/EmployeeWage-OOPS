2eeb13344ade8e835b49696139c746e34dd93755
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-03-16 15:27:59 +0530

    UC2 - Calculate Daily Employee Wage

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,19 @@ public class Main {
 
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        // UC1 - Check Employee is Present or Absent
+        // UC1 & UC2 - Check Employee is Present or Absent and Calculate Daily Wage
+        final int WAGE_PER_HOUR = 20;
+        final int FULL_DAY_HOURS = 8;
+
         int empCheck = (int)(Math.random() * 2); // 0 or 1
 
         if (empCheck == 1) {
             System.out.println("Employee is Present");
+            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+            System.out.println("Daily Employee Wage: " + dailyWage);
         } else {
             System.out.println("Employee is Absent");
+            System.out.println("Daily Employee Wage: 0");
         }
 
     }
